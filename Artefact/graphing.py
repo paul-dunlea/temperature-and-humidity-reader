9129444bc1364483bd9ed81811e38258443038c7
Paul Dunlea <59515552+paul-dunlea@users.noreply.github.com>
2024-09-10 15:54:52 +0100

    Add files via upload

--- Artefact/graphing.py
+++ Artefact/graphing.py
@@ -3,18 +3,31 @@ import matplotlib.pyplot as plt
 #imports and uses csv reader function
 import csv
 with open('sen.csv', 'r') as f:
-    temp = list(csv.reader(f, delimiter=';'))
-    print(temp[:])
-temp = np.genfromtxt("sen.csv", delimiter=",", dtype="float")#assigns a float values as default data types
+    readings = list(csv.reader(f, delimiter=';'))
+    print(readings[:])
+readings = np.genfromtxt("sen.csv", delimiter=",", dtype="float")#assigns a float values as default data types
 
-print('Shape: ', temp.shape)
-print('Datatype: ', temp.dtype)
-print('Size: ', temp.size)
+print('Shape: ', readings.shape)
+print('Datatype: ', readings.dtype)
+print('Size: ', readings.size)
 
 import matplotlib.pyplot as plt
+temp=[]
+humidity=[]
+for i in range(len(readings)):
+    if i%2==0:
+        temp.append(readings[i])
+    else:
+        humidity.append(readings[i])
+
 plt.plot(temp)
 plt.title("Temperature Reading")
 plt.xlabel("")
 plt.ylabel("Temp")
+plt.show()
+plt.plot(humidity)
+plt.title("Humidity Reading")
+plt.xlabel("")
+plt.ylabel("Humidity")
 plt.show()
 
